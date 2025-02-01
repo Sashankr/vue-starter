cd224c2bb86e5b481c6780385aa62d2f1112a120
Sashankr <sashankr97@gmail.com>
2025-02-01 23:30:25 +0530

    Learned to include vue cdn, createApp which takes data function and method functions, also updated markup in html

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-function addItem() {
-  const content = inputEl.value;
-  const newItem = document.createElement("li");
-  newItem.textContent = content;
-  listEl.appendChild(newItem);
-  inputEl.value = "";
-}
+// function addItem() {
+//   const content = inputEl.value;
+//   const newItem = document.createElement("li");
+//   newItem.textContent = content;
+//   listEl.appendChild(newItem);
+//   inputEl.value = "";
+// }
 
-buttonEl.addEventListener("click", addItem);
+// buttonEl.addEventListener("click", addItem);
+
+Vue.createApp({
+  data() {
+    return {
+      currentInput: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.currentInput);
+      this.currentInput = "";
+    },
+  },
+}).mount("#app");
